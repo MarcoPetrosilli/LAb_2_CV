3d5deaf406a9dd32f5f88c1d80c44709d00d372d
GiorgioMarmolino <giorgio.marmolino@gmail.com>
2024-10-03 16:47:04 +0200

    Update Noise.m

--- Noise.m
+++ Noise.m
@@ -1,27 +1,56 @@
 %Add Gaussian (standard deviation=20) and salt & pepper (density=20%) noise to the provided 
 %images: display the images, the noisy images, and their histograms.
 
+
+img_in = imread("Lab2_testimages\tree.png"); 
+
+figure
+imagesc(img_in);
+colormap gray
+
 %% gaussian noise
+std_dev = 20;
 
-%%%%noise
-load tree
-figure,imagesc(IN),colormap gray
+img_out=double(img_in)+std_dev*randn(size(img_in));
 
-%normal distribution
-out=double(IN)+10*randn(size(IN));%10 is the standard deviation
-figure,imagesc(out),colormap gray
-out=double(IN)+30*randn(size(IN));%30 is the standard deviation
-figure,imagesc(out),colormap gray
-%about noise distribution
-a=randn(1,100);
-figure,hist(a,21)
+figure
+subplot(1,2,1)
+imagesc(img_out)
+colormap gray
+title("Gaussian noise")
 
-%Salt and pepper 
-IN=double(IN);
-[rr,cc]=size(IN);
-maxv=max(max(IN));
-indices=full(sprand(rr,cc,0.3));%0.3 is the noise density 
-mask1=indices>0 & indices<0.5;  mask2=indices>=0.5;%matlab masking technique
-out= IN.*(~mask1) ;
-out=out.*(~mask2)+maxv*mask2;
-figure,imagesc(out),colormap gray
+% histogram
+% figure
+subplot(1,2,2)
+histogram(img_out, 256)
+title("Noise distrubution");
+
+%% Salt and pepper 
+density = 0.2;
+
+img_in=double(img_in);
+[raws,cls]=size(img_in);
+maxv=max(max(img_in));
+
+img_indices=full(sprand(raws,cls,density));
+
+mask1=img_indices>0 & img_indices<0.5;  %pepper
+mask2=img_indices>=0.5; %salt
+
+img_out= img_in.*(~mask1) ;
+img_out=img_out.*(~mask2)+maxv*mask2;
+
+figure
+subplot(1,2,1)
+imagesc(img_out)
+colormap gray
+title("Salt & pepper")
+
+% histogram
+% figure
+subplot(1,2,2)
+histogram(img_out, 256)
+title("Salt & pepper distribution")
+
+
+
